62034897228712c548ad2282b72f780125679276
ERICK ALFREDO CIELO <ERICK ALFREDO CIELO>
2015-10-25 00:56:41 -0500

    cambios realizados por primera vez

---
+++ fup1c/calificacion.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+
+int cali;
+
+int main()
+{
+	printf("programa que te diga si aprobaste o no\n");
+	printf("introduce tu calificacion   ");
+	scanf("%d",&cali);
+	printf("tu calificacion es: %d\n",cali);
+	if(cali >= 7)
+{
+
+
+	printf("aprobado\n");
+
+}
+    else
+{
+	printf("reprobado");
+}
+
+}
